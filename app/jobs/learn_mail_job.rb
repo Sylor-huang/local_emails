a0ef7dcdf6c8c69dae5d0b5b4fa3c3bb54a7eb2a
Udasker <udasker@126.com>
2019-09-13 08:50:02 +0800

    some change

--- app/jobs/learn_mail_job.rb
+++ app/jobs/learn_mail_job.rb
@@ -4,11 +4,11 @@ class LearnMailJob < ApplicationJob
   def perform(mail)
     begin
       LearnMailer.learn_send(mail).deliver_now
-      mail.update_attribute(:is_send, true)
+      mail.update_attribute(:is_send, 1) #0未发送，1发送成功，2发送失败
     rescue Net::SMTPAuthenticationError, Net::SMTPServerBusy, Net::SMTPSyntaxError, Net::SMTPFatalError, Net::SMTPUnknownError => e
 
       Rails.logger.info("####__________#######{e}")
-      mail.update_attribute(:is_send, true)
+      mail.update_attribute(:is_send, 2)
     end
     # Do something later
   end
